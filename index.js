a82f34119d4d9e34dda2107b99ae2bdab6954568
mwangiowennjugunasmith <owen.njugun@student.moringa.com>
2023-09-24 23:25:31 +0300

    dOne

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+ 
+  function mondayWork(task = "go to the office") {
+    return `This Monday, I will ${task}.`;
+  }
+  
+function wrapAdjective(visualFlair = "*") {
+    
+    return function (adjective = "special") {
+      return `You are ${visualFlair}${adjective}${visualFlair}!`; 
+    };
+  }
+  
+  
+ 
+  const encouragingPromptFunction = wrapAdjective("!!!");
+  
+  
+  console.log(encouragingPromptFunction()); 
+  console.log(encouragingPromptFunction("amazing")); 
+  
